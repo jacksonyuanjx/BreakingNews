06d798b6e3be3aa93f283e2e77fa3599fe1f9252
Feng Xiong <fengxiong34@gmail.com>
2021-01-09 18:03:03 -0800

    set up for phone view

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import React, { useEffect, useState } from "react";
+import styled, { ThemeProvider } from "styled-components";
+import theme from "./theme";
+
+const StyledApp = styled.div`
+  position: relative;
+  max-width: 480px;
+  margin: 0 auto;
+  background: #ededed;
+  height: ${(props) => props.height + "px"};
+`;
+
 function App() {
-  return <div className="App">hello</div>;
+  const [height, setHeight] = useState(window.innerHeight);
+
+  const onResize = () => {
+    setHeight(window.innerHeight * 0.01);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", onResize);
+    return window.removeEventListener("resize", onResize);
+  });
+  return (
+    <ThemeProvider theme={theme}>
+      <StyledApp height={height}></StyledApp>
+    </ThemeProvider>
+  );
 }
 
 export default App;
